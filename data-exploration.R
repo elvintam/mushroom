4b9fae96c5597466fff1f402b90b3c11538a2211
elvintam <siochi@gmail.com>
2021-04-14 14:17:54 +0800

    factor as.numeric

--- data-exploration.R
+++ data-exploration.R
@@ -1,9 +1,32 @@
 library(tidyverse)
 library(caret)
 
-summary(train_set)
-str(train_set)
+summary(mushroom) 
+str(mushroom) 
 
+sapply(mushroom, function(x) {length(unique(x))} )
+
+mushroom <- mushroom %>% select(-veil_type, -stalk_root)
+
+set.seed(12345, sample.kind="Rounding")
+test_index <- createDataPartition(y = mushroom$class, times = 1,
+                                  p = 0.2, list = FALSE)
+test_set <- mushroom[test_index,]
+train_set <- mushroom[-test_index,]
+
+rm(test_index)
+
+
+cor(as.integer(mushroom[]))
+
+class(mushroom)
+
+
+transform()
+
+t <- transform(mushroom, class=as.numeric(as.character(mushroom)))
+
+t
 
 featurePlot(x = train_set[, 1:23],
             y = train_set$class,
@@ -13,3 +36,5 @@ featurePlot(x = train_set[, 1:23],
                           y = list(relation="free")))
 
 help("featurePlot")
+
+
